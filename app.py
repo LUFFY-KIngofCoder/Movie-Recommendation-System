5224bf4b483c988296b10d79a96a0edc7295e48b
LUFFY-KIngofCoder <65397647+LUFFY-KIngofCoder@users.noreply.github.com>
2025-02-02 02:56:54 +0530

    beautify

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pickle
 import requests
-from streamlit import columns
 
+# Set up page title
 st.title("Movie Recommendation System")
 
-new_df = pickle.load(open("movies.pkl" ,"rb"))
-similarity = pickle.load(open("similarity.pkl" ,"rb"))
+# Load movie data and similarity matrix
+new_df = pickle.load(open("movies.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
+# Extract the list of movie titles
 movie_list = new_df.title.values
 
+
+# Function to fetch movie poster from TMDB API
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
     headers = {
@@ -21,49 +25,53 @@ def fetch_poster(movie_id):
     poster_path = data['poster_path']
     return f"https://image.tmdb.org/t/p/w500/{poster_path}"
 
+
+# Function to recommend movies based on the selected movie
 def recommendation(movie):
     index = new_df[new_df["title"] == movie].index[0]
     distances = similarity[index]
-    top5 = sorted(list(enumerate(distances)), key = lambda x: x[1],reverse=True)[1:6]
+    top5 = sorted(list(enumerate(distances)), key=lambda x: x[1], reverse=True)[1:6]
 
-    recommended_name = []
-    recommended_poster = []
+    recommended_names = []
+    recommended_posters = []
+
     for i in top5:
         movie_id = new_df.iloc[i[0]].movie_id
-        recommended_poster.append(fetch_poster(movie_id))
-        recommended_name.append(new_df.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+        recommended_names.append(new_df.iloc[i[0]].title)
 
-    return recommended_poster,recommended_name
+    return recommended_posters, recommended_names
 
 
+# User input: Select a movie from the list
 option = st.selectbox(
     "Search name of the movie",
     movie_list,
-    index=None,
-    placeholder="Select contact method...",
+    placeholder="Select a movie...",
 )
 
-button = st.button("Recommendations")
+# Button to trigger recommendations
+button = st.button("Get Recommendations")
 
 if button:
-    col1, col2, col3, col4, col5 = st.columns(5 , vertical_alignment="bottom")
-    col = [col1, col2, col3, col4, col5]
-    recommended_poster,recommended_name = recommendation(option)
+    # Layout for displaying recommended movies
+    col1, col2, col3, col4, col5 = st.columns(5, vertical_alignment="bottom")
+    cols = [col1, col2, col3, col4, col5]
+    recommended_posters, recommended_names = recommendation(option)
 
+    # Display recommended movies
     for i in range(5):
-        with col[i]:
-            st.text(recommended_name[i])
-            st.image(recommended_poster[i])
+        with cols[i]:
+            st.text(recommended_names[i])
+            st.image(recommended_posters[i])
 
+# Layout for displaying random movies
+cl1, cl2, cl3, cl4, cl5 = st.columns(5, vertical_alignment="top")
+columns_random = [cl1, cl2, cl3, cl4, cl5]
 
-
-
-
-cl1 , cl2 , cl3 , cl4, cl5 = st.columns(5, vertical_alignment="top")
-columnss = [cl1 , cl2 , cl3 , cl4, cl5]
 for i in range(2):
     rand_movies = new_df["movie_id"].sample(5).values
-    for i in range(5):
-        with columnss[i]:
-            st.text(new_df[new_df["movie_id"] == rand_movies[i]].title.values[0])
-            st.image(fetch_poster(rand_movies[i]))
+    for j in range(5):
+        with columns_random[j]:
+            st.text(new_df[new_df["movie_id"] == rand_movies[j]].title.values[0])
+            st.image(fetch_poster(rand_movies[j]))
